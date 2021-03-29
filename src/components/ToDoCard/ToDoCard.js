9918de9fabcf610a4c8a0bbb20e36c9e2833b810
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-03-29 12:44:51 +0200

    Add bg colour to dragged items

--- src/components/ToDoCard/ToDoCard.js
+++ src/components/ToDoCard/ToDoCard.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import { FaCheck, FaTimes } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
 import { Draggable } from 'react-beautiful-dnd';
-
-import classes from './ToDoCard.module.css';
+import styled from 'styled-components';
 
 export default function ToDoCard({ content, index, passedId }) {
+	const Container = styled.div`
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: auto;
+		padding: 4px 10px 4px 10px;
+		background-color: ${(props) => (props.isDragging ? '#efeae1' : 'white')};
+		border-radius: 4px;
+		margin-bottom: 10px;
+		border: 2px solid transparent;
+		box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		-webkit-box-sizing: border-box;
+
+		&:hover {
+			background-color: #faf9f6;
+		}
+	`;
+
 	return (
 		<Draggable draggableId={passedId} index={index}>
-			{(provided) => (
-				<div
-					className={classes.ToDoCard}
+			{(provided, snapshot) => (
+				<Container
 					{...provided.draggableProps}
 					{...provided.dragHandleProps}
 					ref={provided.innerRef}
+					isDragging={snapshot.isDragging}
 				>
 					<p>{content}</p>
 					<FaTimes style={{ cursor: 'pointer' }} />
-				</div>
+				</Container>
 			)}
 		</Draggable>
 	);
