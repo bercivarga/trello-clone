7667556e678aad834083034313f054a621af1bb4
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-03-30 18:07:41 +0200

    Make it possible to add new tasks

--- src/components/CardColumn/CardColumn.js
+++ src/components/CardColumn/CardColumn.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDoCard from '../ToDoCard/ToDoCard';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
+
+import { useGlobalContext } from '../../context';
 
 import classes from './CardColumn.module.css';
 
@@ -11,7 +13,68 @@ const TaskList = styled.div`
 	padding: 20px;
 `;
 
+const NewTaskBtn = styled.div`
+	width: calc(100% - 40px);
+	margin: 10px auto 20px auto;
+	cursor: pointer;
+
+	&:hover {
+		color: grey;
+	}
+`;
+
+const Form = styled.form`
+	width: calc(100% - 40px);
+	margin: 0 auto 20px auto;
+`;
+
+const Input = styled.input`
+	width: 98%;
+	height: 24px;
+	border: 1px solid grey;
+	border-radius: 4px;
+`;
+
 export default function CardColumn({ column, tasks, index }) {
+	const [ showForm, setShowForm ] = useState(false);
+	const [ newTask, setNewTask ] = useState('');
+
+	const { data, setData } = useGlobalContext();
+
+	const changeShowForm = () => {
+		setShowForm(!showForm);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (newTask === '') {
+			return;
+		}
+
+		const newTasks = Object(data.tasks);
+		newTasks[newTask] = {
+			id: newTask,
+			content: newTask
+		};
+
+		const newTaskIds = Array.from(data.columns[column.id].taskIds);
+		newTaskIds.push(newTask);
+
+		const newColumns = Object(data.columns);
+		newColumns[column.id].taskIds = newTaskIds;
+
+		const newState = {
+			...data,
+			tasks: newTasks,
+			columns: newColumns
+		};
+
+		setData(newState);
+		setNewTask('');
+		setShowForm(false);
+		return;
+	};
+
 	return (
 		<Draggable draggableId={column.id} index={index}>
 			{(provided) => (
@@ -34,6 +97,10 @@ export default function CardColumn({ column, tasks, index }) {
 							</TaskList>
 						)}
 					</Droppable>
+					<NewTaskBtn onClick={changeShowForm}>Add new task +</NewTaskBtn>
+					<Form onSubmit={handleSubmit} style={{ display: `${showForm ? 'block' : 'none'}` }}>
+						<Input type="text" name="newtask" onChange={(e) => setNewTask(e.target.value)} />
+					</Form>
 				</div>
 			)}
 		</Draggable>
--- src/components/ToDoCard/ToDoCard.js
+++ src/components/ToDoCard/ToDoCard.js
@@ -3,26 +3,26 @@ import { FaTimes } from 'react-icons/fa';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
+const Container = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: auto;
+	padding: 4px 10px 4px 10px;
+	background-color: ${(props) => (props.isDragging ? '#efeae1' : 'white')};
+	border-radius: 4px;
+	margin-bottom: 10px;
+	border: 2px solid transparent;
+	box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+
+	&:hover {
+		background-color: #efeae1;
+	}
+`;
+
 export default function ToDoCard({ content, index, passedId }) {
-	const Container = styled.div`
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		height: auto;
-		padding: 4px 10px 4px 10px;
-		background-color: ${(props) => (props.isDragging ? '#efeae1' : 'white')};
-		border-radius: 4px;
-		margin-bottom: 10px;
-		border: 2px solid transparent;
-		box-sizing: border-box;
-		-moz-box-sizing: border-box;
-		-webkit-box-sizing: border-box;
-
-		&:hover {
-			background-color: #efeae1;
-		}
-	`;
-
 	return (
 		<Draggable draggableId={passedId} index={index}>
 			{(provided, snapshot) => (
