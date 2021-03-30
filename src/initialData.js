35d9a53549252ad41fad223549e1bc629cba3a02
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-03-30 18:39:39 +0200

    Add funny tasks and different order

--- src/initialData.js
+++ src/initialData.js
@@ -1,25 +1,26 @@
 const initialData = {
 	tasks: {
 		'task-1': { id: 'task-1', content: 'Take out the garbage' },
-		'task-2': { id: 'task-2', content: 'Watch my favorite show' },
-		'task-3': { id: 'task-3', content: 'Charge my phone' },
-		'task-4': { id: 'task-4', content: 'Cook dinner' }
+		'task-2': { id: 'task-2', content: 'Work on my projects' },
+		'task-3': { id: 'task-3', content: 'Reach world domination' },
+		'task-4': { id: 'task-4', content: 'Cook dinner' },
+		'task-5': { id: 'task-5', content: 'Play some Geoguessr' }
 	},
 	columns: {
 		'column-1': {
 			id: 'column-1',
 			title: 'To do',
-			taskIds: [ 'task-1', 'task-2', 'task-3', 'task-4' ]
+			taskIds: [ 'task-2', 'task-4', 'task-5' ]
 		},
 		'column-2': {
 			id: 'column-2',
 			title: 'In progress',
-			taskIds: []
+			taskIds: [ 'task-3' ]
 		},
 		'column-3': {
 			id: 'column-3',
 			title: 'Done',
-			taskIds: []
+			taskIds: [ 'task-1' ]
 		}
 	},
 	// Facilitate reordering of the columns
